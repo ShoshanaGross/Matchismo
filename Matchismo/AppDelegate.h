cd7746bfe284ed34dec927a5a927ca85c7feca23
Shoshana Gross <shoshana@shoshana-imac.local>
2018-03-05 12:08:07 +0200

    Initial Commit

---
+++ Matchismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by Shoshana Gross on 05/03/2018.
+//  Copyright © 2018 Lightricks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Matchismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by Shoshana Gross on 05/03/2018.
+//  Copyright © 2018 Lightricks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
